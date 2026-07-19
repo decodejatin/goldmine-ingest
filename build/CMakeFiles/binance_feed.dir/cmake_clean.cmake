03f339b5e9372752907192a10d6c876126a392b0
JATIN_JALANDHRA <decodejatin@users.noreply.github.com>
2026-07-19 09:27:15 +0530

    Add unit tests and validation for build/CMakeFiles/progress.marks

---
+++ build/CMakeFiles/binance_feed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binance_feed.dir/src/binance_feed.cpp.o"
+  "CMakeFiles/binance_feed.dir/src/binance_feed.cpp.o.d"
+  "binance_feed"
+  "binance_feed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binance_feed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
